a9d4dd7f98438f93c5ab14a60cc501b3669b9a33
none <clipsquickwork@gmail.com>
2026-01-30 14:25:14 +0200

    feat: add Paddle.js provider component

---
+++ src/components/PaddleProvider.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useEffect } from 'react'
+import Script from 'next/script'
+
+declare global {
+  interface Window {
+    Paddle: any
+  }
+}
+
+interface PaddleProviderProps {
+  children: React.ReactNode
+}
+
+export function PaddleProvider({ children }: PaddleProviderProps) {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.Paddle) {
+      if (process.env.NEXT_PUBLIC_PADDLE_ENV === 'sandbox') {
+        window.Paddle.Environment.set('sandbox')
+      }
+      window.Paddle.Initialize({
+        token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN,
+      })
+    }
+  }, [])
+
+  return (
+    <>
+      <Script
+        src="https://cdn.paddle.com/paddle/v2/paddle.js"
+        onLoad={() => {
+          if (process.env.NEXT_PUBLIC_PADDLE_ENV === 'sandbox') {
+            window.Paddle.Environment.set('sandbox')
+          }
+          window.Paddle.Initialize({
+            token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN,
+          })
+        }}
+      />
+      {children}
+    </>
+  )
+}
